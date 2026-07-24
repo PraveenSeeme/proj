377777579453d407fa9c101ed35b2615b6797028
PraveenSeeme <seemepraveen77@gmail.com>
2026-07-24 06:06:14 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ echo "Install Zip, Unzip and nginx"
 sudo apt install nginx zip unzip
 
 echo "Remove nginx html page"
-sudo rm -r /var/www/index/*.html
+sudo rm -r /var/www/html/*.html
 
 echo "copy file"
 sudo git clone https://github.com/PraveenSeeme/proj.git /var/www/html
